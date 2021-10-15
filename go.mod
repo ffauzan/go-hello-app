1dd96506a9d79d328632aae2792373e8fd7d284c
Ffauzan <f.fauzan@mail.ugm.ac.id>
2021-10-15 17:25:47 +0700

    just a commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ffauzan/go-hello-app
 
 go 1.17
+
+require github.com/ffauzan/go-hello-mod v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	say_hello "github.com/ffauzan/go-hello-mod"
+)
+
+func main() {
+	say_hello.SayHello("falfal")
+}
